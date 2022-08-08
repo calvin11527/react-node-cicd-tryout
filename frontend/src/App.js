0220292ac2a10f6454f32d6465f6cbb5954b8b82
goatcheese93178 <calvin11527@hotmail.com>
2022-08-09 01:00:13 +0800

    update azure deployment yml

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,24 +1,24 @@
-import React, { useEffect, useState } from 'react';
-import axios from 'axios';
-import './App.css';
-const GetNamesAPIPath = process.env.REACT_APP_API_GETNAMES;
+import React, { useEffect, useState } from 'react'
+import axios from 'axios'
+import './App.css'
+const GetNamesAPIPath = process.env.REACT_APP_API_GETNAMES
 
 function App() {
-  const [username, setUsername] = useState('');
+  const [username, setUsername] = useState('')
   useEffect(() => {
-    getNames();
-  }, []);
+    getNames()
+  }, [])
 
   const getNames = async () => {
-    const res = await axios.get(GetNamesAPIPath);
-    setUsername(res.data);
+    const res = await axios.get(GetNamesAPIPath)
+    setUsername(res.data)
   }
   return (
     <>
       <h1>Frontend</h1>
       <h3>{username}</h3>
     </>
-  );
+  )
 }
 
-export default App;
+export default App
