789727506afc98e83fdc988a5e4b66b6cbdbedc9
calvinli <kingyiuli@skhwc.org.hk>
2022-08-08 15:44:37 +0800

    init commit

---
+++ frontend/src/App.js
@@ -0,0 +1,24 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import './App.css';
+const GetNamesAPIPath = process.env.REACT_APP_API_GETNAMES;
+
+function App() {
+  const [username, setUsername] = useState('');
+  useEffect(() => {
+    getNames();
+  }, []);
+
+  const getNames = async () => {
+    const res = await axios.get(GetNamesAPIPath);
+    setUsername(res.data);
+  }
+  return (
+    <>
+      <h1>Frontend</h1>
+      <h3>{username}</h3>
+    </>
+  );
+}
+
+export default App;
